5b0e442a308aa08b8171d0e1acfe9f511d451e0c
akshay.dhondge <akshay.dhondge@cuelogic>
2020-05-21 19:16:30 +0530

    21st May EOD Commit

--- app/src/main/java/com/example/multiplescreensizesample/MainActivity.kt
+++ app/src/main/java/com/example/multiplescreensizesample/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.example.multiplescreensizesample
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.marginTop
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+
+            btnShowToast.setOnClickListener {
+                val txvWelcome = findViewById<TextView>(R.id.txvWelcome)
+
+                Log.i("Main Activity", "{marginValue: ${txvWelcome.height}")
+                Toast.makeText(this, "${txvWelcome.height}", Toast.LENGTH_SHORT).show()
+
+            }
+        } else {
+            btnShowToast.setOnClickListener {
+                Toast.makeText(this, "we are in Landscape mode", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
     }
 }
